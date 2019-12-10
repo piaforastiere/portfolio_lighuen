01d8c930f30f91e036ea0b27668828b1581e0aa8
GabrieleM <gabriele.merra@gmail.com>
2019-12-10 21:12:23 +0100

    rimossi file non utili

--- pages/assets/js/singular_swiper.js
+++
@@ -1,9 +0,0 @@
-import React from 'react' // that's it
-import ReactDOM from 'react-dom'
-
-document.addEventListener('DOMContentLoaded', function() {
-  ReactDOM.render(
-  <SingularSwiper />, document.getElementById('singular_swiper')
-  )
-
-}, false);
